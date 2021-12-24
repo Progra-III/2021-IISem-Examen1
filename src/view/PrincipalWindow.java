2a05adf353c54fade55db0db3ffb3b0f13ecd851
Francisco_Arguedas <farguedas99@gmail.com>
2021-12-24 13:47:40 -0600

    Correcion MVC

--- src/view/PrincipalWindow.java
+++ src/view/PrincipalWindow.java
@@ -1,6 +1,12 @@
 package view;
 
+import controller.ActorController;
+import controller.CinemaController;
+import controller.DirectorController;
+import controller.PrincipalController;
+
 import javax.swing.*;
+import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class PrincipalWindow extends JFrame{
@@ -11,7 +17,6 @@ public class PrincipalWindow extends JFrame{
     private JPanel windowPanel;
 
     public PrincipalWindow(){
-
         setContentPane(windowPanel);
         setTitle("Menu Principal");
         setSize(900,500);
@@ -19,25 +24,51 @@ public class PrincipalWindow extends JFrame{
         setResizable(false);
         setLocationRelativeTo(null);
         setButtons();
-    }
 
-    public void addListener(ActionListener action){
-        btnActor.addActionListener(action);
-        btnDirector.addActionListener(action);
-        btnMovie.addActionListener(action);
+        ButtonListener listener = new ButtonListener();
+        btnMovie.addActionListener(listener);
+        btnDirector.addActionListener(listener);
+        btnActor.addActionListener(listener);
     }
-
     public void setButtons() {
         btnActor.setActionCommand("1");
         btnDirector.setActionCommand("2");
         btnMovie.setActionCommand("3");
     }
-
     public void displayErrorMessage(String errorMessage){
         JOptionPane.showMessageDialog(windowPanel, errorMessage, " WARNING ERROR", JOptionPane.ERROR_MESSAGE);
     }
-
     public void displayMessage(String message){
         JOptionPane.showMessageDialog(windowPanel, message);
     }
+    private class ButtonListener implements ActionListener {
+
+        private ButtonListener() {
+        }
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            int valor = Integer.parseInt(e.getActionCommand());
+            switch (valor) {
+                case 1:
+                    //ACTOR
+                    ActorController actorController = new ActorController();
+                    dispose();
+
+                    break;
+                case 2:
+                    //DIRECTOR
+                    DirectorController directorController = new DirectorController();
+                    dispose();
+                    break;
+
+                case 3:
+                    //PELÍCULA
+                    CinemaController cinemaController = new CinemaController();
+                    dispose();
+                    break;
+            }
+        }
+
+    }
 }
+
